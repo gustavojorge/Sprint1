40951f89c4caa00227362ead736da4e038087992
gustavojorge <gustavojorge080@gmail.com>
2025-01-24 09:15:51 -0300

    atividade feita

--- bin/trainee_studies.dart
+++ bin/trainee_studies.dart
@@ -1,17 +1,3 @@
 void main(List<String> arguments){
-  int idade = 22;
-  double altura = 1.75;
-  bool geek = true;
-  String nome = 'Gustavo Jorge Novaes Silva';
-  String apelido = 'Guga';
-
-  List<dynamic> dados = [idade, altura, geek, nome, apelido];
-
-  String out = 'Eu sou o ${dados[4]} \n'
-      'mas meu nome completo é: ${dados[3]},\n'
-      'eu me considero geek? ${dados[2]} \n'
-      'Eu tenho ${dados[1]} metros de altura e \n'
-      '${dados[0]} anos de idade';
-
-  print(out);
+  for(int i = 0; i < 5; i++) print("Concluí $i voltas");
 }
